539a400dff64f949cad873c35e02a4474dc8f7a2
Lorenzo Sijinardi <lorenzo.sijinardi@accenture.com>
2025-06-10 14:26:26 +0200

    rimozione search bar da navbar

--- FE/src/scenes/layout/navbar/index.jsx
+++ FE/src/scenes/layout/navbar/index.jsx
@@ -13,7 +13,6 @@ import {
   MenuOutlined,
   NotificationsOutlined,
   PersonOutlined,
-  SearchOutlined,
   SettingsOutlined,
 } from "@mui/icons-material";
 import { ToggledContext } from "../../../App";
@@ -38,18 +37,6 @@ const Navbar = () => {
         >
           <MenuOutlined />
         </IconButton>
-        <Box
-          display="flex"
-          alignItems="center"
-          bgcolor={colors.primary[400]}
-          borderRadius="3px"
-          sx={{ display: `${isXsDevices ? "none" : "flex"}` }}
-        >
-          <InputBase placeholder="Search" sx={{ ml: 2, flex: 1 }} />
-          <IconButton type="button" sx={{ p: 1 }}>
-            <SearchOutlined />
-          </IconButton>
-        </Box>
       </Box>
 
       <Box>
