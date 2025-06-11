bc1d654b13a7ee2b44a607845bdb08bec673fe2b
Lorenzo Sijinardi <lorenzo.sijinardi@accenture.com>
2025-06-11 09:07:13 +0200

    rinominazione tab transazioni

--- BE/controllers/transactionsController.js
+++ BE/controllers/transactionsController.js
@@ -2,7 +2,7 @@ import { db } from "../config/db.js";
 
 export const getAllTransactions = async (req, res) => {
   try {
-    const [rows] = await db.query("SELECT * FROM transactions");
+    const [rows] = await db.query("SELECT * FROM transazioni");
     res.json(rows);
   } catch (error) {
     console.error("Errore durante il recupero delle transazioni:", error);
