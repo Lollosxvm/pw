e070f70fd01e08fded64056017214d66d4beb56e
Lorenzo Sijinardi <lollosxvm@icloud.com>
2025-07-29 18:32:08 +0200

    fix Vercel FE

--- FE/src/api/axios.js
+++ FE/src/api/axios.js
@@ -1,7 +1,5 @@
-import axios from "axios";
-
 export default axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
--- FE/src/api/axiosPrivate.js
+++ FE/src/api/axiosPrivate.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../redux/store";
 
 const axiosPrivate = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
